57221bfda48f4aff3eeb92d9849497603e311e00
brunoredes <bruno.donatelli@etec.sp.gov.br>
2020-11-29 10:45:50 -0300

    Lazy Loading, creating modules and routings

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,27 +4,33 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ProductModule } from './modules/product/product.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/template/header/header.component';
 import { FooterComponent } from './shared/components/template/footer/footer.component';
 import { NavComponent } from './shared/components/template/nav/nav.component';
+import { HomeComponent } from './modules/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ProductModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
